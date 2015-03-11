ce7a094c0e23e3695c87ab9443e5695e729e5839
Daniel Pett <dpett@britishmuseum.org>
2015-03-11 14:09:55 +0000

    Set to correct category

--- createTasks.py
+++ createTasks.py
@@ -141,7 +141,9 @@ def run(app_config, options):
     def setup_app():
         app = find_app_by_short_name()
         app.long_description = contents('long_description.html')
-        app.category_id = 5
+        # Set to Egyptian archaeology
+        app.category_id = 10
+        # Make hidden
         app.hidden = 1
         app.info['task_presenter'] = contents('template.html')
         app.info['thumbnail'] = app_config['thumbnail']
